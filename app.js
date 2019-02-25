a664c4211eb9259ea9fa4a6317386c5e0d6f667e
felixzxk <felixzxk@gmail.com>
2019-02-25 16:36:48 +0800

    feat:添加版本更新检查功能

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { get, post, setToken } from "./utils/request";
 App({
   onLaunch: function(res) {
     this.globalData.initState = res;
+    this.checkUpdate()
     setTimeout(() => {
       this.login();
     }, 1000)
@@ -83,6 +84,28 @@ App({
         url: "/pages/customPage/index?code=home"
       });
     }
+  },
+  checkUpdate: function(){
+    const updateManager = wx.getUpdateManager();
+    // 检查是否有新版本
+    updateManager.onCheckForUpdate(function (res) {
+      if(res.hasUpdate) {
+        // 如果有新版本就下载
+        updateManager.onUpdateReady(function () {
+          // 下载完成后强制更新
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success(r) {
+              if (r.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate();
+              }
+            }
+          })
+        })
+      }
+    })
   },
   globalData: {
     userInfo: null,
